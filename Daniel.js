7c4d3711c3b00726e340dc39211ecc7ef0d3b880
daniel luke <manthid5@gmail.com>
2026-04-23 10:25:46 +0300

    WIP: number generator for challenge three

--- Daniel.js
+++ Daniel.js
@@ -1,1 +1,29 @@
-java
+// Generate random number between 1 and 50
+let randomNum = Math.floor(Math.random() * 50) + 1;
+if (randomNum < 50) {
+  console.log("upper");
+} else {
+  console.log("lower");
+}
+
+
+
+// let studentScore = 81;
+
+// switch (true) {
+//   case studentScore > 79:
+//     console.log("A");
+//     break;
+
+//   case studentScore > 64:
+//     console.log("B");
+//     break;
+
+//   case studentScore > 44:
+//     console.log("D");
+//     break;
+
+//   default:
+//     console.log("F");
+//     break;
+// }
